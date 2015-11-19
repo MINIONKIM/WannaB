2b9d70e6c5d61c41f2cd2f27f4adfcff33ad488a
MINHYEON <201101810@inu.ac.kr>
2015-11-19 20:57:25 +0900

    login

---
+++ app/src/main/java/minion/kim/wannab/LoginConfig.java
@@ -0,0 +1,11 @@
+package minion.kim.wannab;
+
+/**
+ * Created by minion on 2015-11-19.
+ */
+public class LoginConfig {
+
+    public static String URL_LOGIN = "http://meeneeon.ddns.net/android_login_api/login.php";
+    public static String URL_REGISTER = "http://meeneeon.ddns.net/android_login_api/register.php";
+
+}
--- app/src/main/java/minion/kim/wannab/Splash.java
+++ app/src/main/java/minion/kim/wannab/Splash.java
@@ -1,36 +1,19 @@
 package minion.kim.wannab;
 
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class Splash extends Activity{
-    Button login_button;
-    EditText uid, pwd;
-    HttpPost httppost;
-    StringBuffer buf;
-    HttpResponse resp;
-    HttpClient httpclient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)c
+    protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        /*
+
         Handler hd = new Handler();
         hd.postDelayed(new Runnable(){
 
@@ -38,10 +21,6 @@ public class Splash extends Activity{
         public void run(){
                 finish();
             }
-        }, 3000);*/
-
-
-
-
+        }, 3000);
     }
 }
